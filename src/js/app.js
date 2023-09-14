e1a8c12966ad523ff31a2de5ba171905205c5354
roman <usankin.roma@gmail.com>
2023-09-14 15:47:43 +0400

    - adding js and styles for send massage

--- src/js/app.js
+++ src/js/app.js
@@ -298,6 +298,51 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     modal('.modal', 'modal--active', '[data-modal]', '.modal__close');
+    modal('.thanks', 'thanks--active', '[data-thanks]', '.thanks__close');
+
+
+    // показ окошек с благодарностью после отправки форм
+    let success = document.querySelector('.thanks')
+    let forms = document.querySelectorAll('.wpcf7-form')
+    if (forms) {
+        forms.forEach(form => {
+            form.addEventListener('wpcf7mailsent', function (event) {
+                let id = form.parentNode.getAttribute('id');
+                if (id === 'wpcf7-f102-o1'){ //квиз
+                    setTimeout(function () {
+                        window.location.href='/spasibo/'
+                    }, 1000);
+                } else {
+                    modalForm.classList.remove('modal--active');
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+
+                    success.classList.add('active')
+                    setTimeout(function () {
+                        success.classList.remove('active')
+                        document.body.style.overflow = '';
+                        document.body.style.marginRight = '0px';
+                    }, 2000);
+                }
+            }, false)
+        })
+    }
+    if (success) {
+        success.addEventListener('click', () => {
+            success.classList.remove('active')
+            document.body.style.overflow = '';
+            document.body.style.marginRight = '0px';
+        })
+    }
+
+    let phoneInputs = document.querySelectorAll('input[name*="phone"]')
+
+    phoneInputs.forEach(phoneInput => {
+        phoneInput.setAttribute('onkeypress', 'maskPhone(event)')
+        phoneInput.setAttribute('onpaste', 'onPastePhone(event)')
+    })
+
+
     // появление модалки вынести в отдельный файл который будет подклбючаться только на главной странице
     // if (modal){
     //     setTimeout(()=>{
@@ -310,3 +355,64 @@ document.addEventListener('DOMContentLoaded', function () {
         Fancybox.bind("[data-fancybox]", {});
     }
 });
+
+function maskPhone(e) {
+    const mask = /\+7 \(\d{3}\) \d{3} \d{2} \d{2}/;
+    var valSize = e.target.value.trim().replace(/\D/g, "").length;
+    e = e || window.event;
+    var key = e.keyCode || e.which;
+    key = String.fromCharCode(key);
+    var regex = /[0-9]|\+/;
+    if (!regex.test(key)) {
+        e.returnValue = false;
+        if (e.preventDefault) e.preventDefault();
+    } else {
+        if (valSize !== 0 && key === "+") {
+            e.returnValue = false;
+            return;
+        }
+        if (valSize === 0) {
+            if (key === "8" || key === "7") {
+                e.target.value = "+7";
+                e.returnValue = false;
+            } else if (key === "9") {
+                e.target.value = "+7 (9";
+                e.returnValue = false;
+            } else if (key !== "+") {
+                e.target.value = "+7 (9";
+            } else if (key === "+" && e.target.value === "+") {
+                e.returnValue = false;
+            }
+        } else if (valSize === 1) {
+            e.target.value = "+7 (9";
+            if (key === "9") {
+                e.returnValue = false;
+            }
+        } else if (valSize === 4) {
+            if (e.target.value.slice(-1) === ")") {
+                e.target.value = e.target.value.trim() + " ";
+            } else if (e.target.value.slice(-1) === " ") {
+                return;
+            } else e.target.value = e.target.value.trim() + ") ";
+        } else if (valSize === 7 || valSize === 9) {
+            if (e.target.value.slice(-1) === " ") {
+                return;
+            } else e.target.value = e.target.value.trim() + " ";
+        } else if (valSize === 11) {
+            e.returnValue = false;
+        }
+    }
+}
+
+function onPastePhone(e) {
+    e.preventDefault();
+    const mask = /(7|8)(9\d{2})(\d{3})(\d{2})(\d{2})/;
+    var phone = e.clipboardData.getData('text/plain').replace(/\D/g, "");
+    if (!mask.test(phone)) {
+        e.returnValue = false;
+        return;
+    }
+    var matched = phone.match(mask);
+    e.target.value = "+7 (" + matched[2] + ") " + matched[3] + " " + matched[4] + " " + matched[5];
+    e.returnValue = false;
+}
